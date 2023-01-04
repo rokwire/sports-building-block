8dd8063148e709313a817b75af1f66789991073c
Stefan Vitanov <stefan@inabyte.com>
2023-01-04 11:38:28 +0200

    set collection.go and database.go

---
+++ driven/storage/database.go
@@ -0,0 +1,92 @@
+// Copyright 2022 Board of Trustees of the University of Illinois.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package storage
+
+import (
+	"context"
+	"time"
+
+	"github.com/rokwire/logging-library-go/v2/logs"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type database struct {
+	mongoDBAuth  string
+	mongoDBName  string
+	mongoTimeout time.Duration
+
+	logger *logs.Logger
+
+	db       *mongo.Database
+	dbClient *mongo.Client
+
+	sportDefinitions *collectionWrapper
+
+	listeners []Listener
+}
+
+func (m *database) start() error {
+	m.logger.Info("database -> start")
+
+	//connect to the database
+	clientOptions := options.Client().ApplyURI(m.mongoDBAuth)
+	connectContext, cancel := context.WithTimeout(context.Background(), m.mongoTimeout)
+	client, err := mongo.Connect(connectContext, clientOptions)
+	cancel()
+	if err != nil {
+		return err
+	}
+
+	//ping the database
+	pingContext, cancel := context.WithTimeout(context.Background(), m.mongoTimeout)
+	err = client.Ping(pingContext, nil)
+	cancel()
+	if err != nil {
+		return err
+	}
+
+	//apply checks
+	db := client.Database(m.mongoDBName)
+
+	sportDefinitions := &collectionWrapper{database: m, coll: db.Collection("sport-definitions")}
+	err = m.applySportDefinitionsChecks(sportDefinitions)
+	if err != nil {
+		return err
+	}
+
+	//asign the db, db client and the collections
+	m.db = db
+	m.dbClient = client
+
+	m.sportDefinitions = sportDefinitions
+
+	return nil
+}
+
+func (m *database) applySportDefinitionsChecks(sportDefinitions *collectionWrapper) error {
+	m.logger.Info("apply sport definitions checks.....")
+
+	//add org id index
+	err := sportDefinitions.AddIndex(bson.D{primitive.E{Key: "org_id", Value: 1}}, false, false)
+	if err != nil {
+		return err
+	}
+
+	m.logger.Info("accounts check passed")
+	return nil
+}
